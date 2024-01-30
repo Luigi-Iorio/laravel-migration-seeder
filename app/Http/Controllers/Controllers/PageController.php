f46572f49103add8cf8cc61d68effadad9e6962d
Luigi-Iorio <luigi0iorio@gmail.com>
2024-01-30 18:31:11 +0100

    feat: rappresentazione treni in partenza dalla data odierna

--- app/Http/Controllers/Controllers/PageController.php
+++ app/Http/Controllers/Controllers/PageController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('home');
+
+        $treni = Train::where('departure_day', '>=', Train::raw('curdate()'))->get();
+        return view('home', compact('treni'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,8 +10,39 @@
 </head>
 
 <body>
+    {{-- main --}}
     <main>
+        {{-- titolo --}}
+        <h1>Treni in partenza dalla data odierna</h1>
+        {{-- /titolo --}}
+        {{-- lista treni --}}
+        <ul>
+            @foreach ($treni as $treno)
+                <li class="card">
+                    <h2>Company: {{ $treno['company'] }}</h2>
+                    <p>Departure Station: {{ $treno['departure_station'] }} </p>
+                    <p>Arrival Station: {{ $treno['arrival_station'] }} </p>
+                    <p>Departure Time: {{ $treno['departure_time'] }}</p>
+                    <p>Arrival Time: {{ $treno['arrival_time'] }}</p>
+                    <p>Departure Day: <span>{{ $treno['departure_day'] }}</span></p>
+                    <p>Train Code: {{ $treno['train_code'] }}</p>
+                    <p>Number of carriages: {{ $treno['num_carriages'] }}</p>
+                    @if ($treno['is_in_time'] === 1)
+                        <p>Is in time: true</p>
+                    @else
+                        <p>Is in time: false</p>
+                    @endif
+                    @if ($treno['is_deleted'] === 1)
+                        <p>Is deleted: true</p>
+                    @else
+                        <p>Is deleted: false</p>
+                    @endif
+                </li>
+            @endforeach
+        </ul>
+        {{-- /lista treni --}}
     </main>
+    {{-- /main --}}
 </body>
 
 </html>
